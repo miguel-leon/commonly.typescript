9c0d5575afba29b4495ed0d4804ad356cccd33f9
Miguel Leon <miguel.e.leon.f@gmail.com>
2021-11-26 10:51:00 -0300

    ⚡️ remove type from proxy properties, leave as string. There are 13699 possible values so the compiler is left dumbfounded. Listing all possible values is inefficient. A better approach would be to validate the property name based on its value instead of against a complete list, but it is not possible to reference itself in its own type annotation, a dead end until something like [k: ValidFlag<typeof k>] would work.

--- src/templates/regexp.ts
+++ src/templates/regexp.ts
@@ -13,29 +13,9 @@ function _regexp(flags: string, strings: TemplateStringsArray, ...keys: string[]
 }
 
 
-type Flag = 'd' | 'g' | 'i' | 'm' | 's' | 'u' | 'y';
-// There are 13699 possible permutations of all sizes
-
-
-type Generate<Concat extends string, Next extends Flag, Rest extends Flag> =
-	Next extends never ?
-		never :
-		(Append<`${ Concat }${ Next }`, Exclude<Rest, Next>>);
-
-type Append<Concat extends string, Rest extends Flag> =
-	Concat | Generate<Concat, Rest, Rest>;
-
-type AllSizesPermutations<F extends Flag> =
-	F extends never ?
-		never :
-		Append<F, Exclude<Flag, F>>;
-
-type Flags = AllSizesPermutations<Flag>;
-
-
 const no_flags_arg = _regexp.bind(undefined, '');
 
 
-export const regexp: typeof no_flags_arg & { [k in Flags]: typeof no_flags_arg } = new Proxy(no_flags_arg, {
+export const regexp: typeof no_flags_arg & { [k: string]: typeof no_flags_arg } = new Proxy(no_flags_arg, {
 	get: (target, property: string) => _regexp.bind(undefined, property)
 }) as any;
