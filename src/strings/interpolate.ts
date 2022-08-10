1bb269e194df21bd96c5de61128ef5bedb692d1a
Miguel Leon <miguel.e.leon.f@gmail.com>
2022-08-10 04:06:14 -0400

    📛 expose strings.interpolate regexp matcher.

--- src/strings/interpolate.ts
+++ src/strings/interpolate.ts
@@ -1,6 +1,10 @@
 export function interpolate(template: string, context: { [k: string]: any }): string {
 	return template.replace(
-		/(?<!\\)((?:\\\\)*)\${(.*?)(?<!\\)(?:\\\\)*}/g,
+		interpolate.matcher,
 		(_, backslashes, key) => backslashes + (context[key.trim()] ?? (() => { throw new TypeError('Missing keys in context') })())
 	);
+}
+
+export namespace interpolate {
+	export const matcher = /(?<!\\)((?:\\\\)*)\${(.*?)(?<!\\)(?:\\\\)*}/g;
 }
